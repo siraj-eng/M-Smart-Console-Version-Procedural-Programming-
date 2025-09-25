29683fc04243f75eba73a77a92d7a674e4f41396
siraj-Eng <wahhajsiraj16@gmail.com>
2025-09-25 11:12:47 +0300

    progress

--- Program.cs
+++ Program.cs
@@ -2,5 +2,33 @@
 
 namespace Msmart
 {
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n========= Welcome to M-Smart Budgeting App =========");
+            Console.WriteLine("\n Helping you stay on top of your money.");
+            Console.WriteLine(" Never get caught without cash again!\n");
 
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine(" 1. Add a Transaction");
+            Console.WriteLine(" 2. View Balance");
+            Console.WriteLine(" 3. View Reports");
+            Console.WriteLine(" 4. Enter Budget");
+            Console.Write("\nYour choice: ");
+        }
+
+        static void AddTransaction()
+        {
+            // Welcoming message
+            Console.WriteLine("\n=== Add a Transaction ===");
+            Console.WriteLine("Welcome to M-Smart Transactions!");
+            Console.WriteLine("Paste your M-Pesa message below,");
+            Console.WriteLine("and we’ll process it for you.\n");
+
+            Console.Write("Paste here: ");
+            string message = Console.ReadLine();
+        }
+    }
 }
